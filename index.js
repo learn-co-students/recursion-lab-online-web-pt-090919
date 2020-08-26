dc3e84886d659199c2ce718fd1929319788571a2
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-08-26 16:46:44 -0400

    write printString and reverseString recursive functions

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
-// Code your solution here!
+// prints out all of the characters in a passed-in string
+// recurses, calling itself once for each letter in the string
+function printString(myString) {
+  console.log(myString[0]);
+
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    // complete once there are no remaining letters to shift to the left
+    return true;
+  }
+}
+
+// reverses all of the letters in a string
+function reverseString(myString) {
+  if (myString.length <= 1) {
+    return myString;
+  } else {
+    return reverseString(myString.substring(1)) + myString[0];
+  }
+  // for (var i = myString.length - 1; i >= 0; i--) {
+  //   newString += myString[i]
+  // }
+  // return newString
+}
+
+// returns 'true' when a string is a palindrome
+function isPalindrome() {
+
+}
+
+function maxOf() {
+
+
+}
+function addUpTo() {
+
+}
+
+function includesNumber() {
+
+}
 
